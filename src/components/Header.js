bc325356711b6c0da32ed3642ac3342885dcb73c
victormmorales <victor.morales@criptan.com>
2022-03-24 13:57:51 +0100

    header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,22 @@
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary justify-content-between">
       <div className="container">
         <h1>
-          <a href="/" className="text-light">
+          <Link to="/" className="text-light">
             CRUD - React, Redux, REST API & Axios
-          </a>
+          </Link>
         </h1>
       </div>
 
-      <a
-        href="/productos/nuevo"
+      <Link
+        to="/productos/nuevo"
         className="btn btn-danger nuevo-post d-block d-md-inline-block"
       >
         Agregar Producto &#43;
-      </a>
+      </Link>
     </nav>
   );
 };
